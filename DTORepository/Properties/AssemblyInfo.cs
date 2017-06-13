0a093341041e3f1e8eaddfae31716f2de94bf828
Theerapat Chawannakul <tchawann@usc.edu>
2017-06-13 13:22:43 -0700

    Fixed namespace cannot be referred to.

--- DTORepository/Properties/AssemblyInfo.cs
+++ DTORepository/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DTORepository")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Mapping DTO to/from Entity with Generic Repository Pattern.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Theerapat Chawannakul")]
 [assembly: AssemblyProduct("DTORepository")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © Theerapat Chawannakul 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,15 +22,15 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("91e0c751-c3be-4fba-843f-702fa2350fb1")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//// Version information for an assembly consists of the following four values:
+////
+////      Major Version
+////      Minor Version 
+////      Build Number
+////      Revision
+////
+//// You can specify all the values or you can default the Build and Revision Numbers 
+//// by using the '*' as shown below:
+//// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
